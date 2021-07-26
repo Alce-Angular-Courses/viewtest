6595b98e90541c6c6ff4251b60f485e434ee387f
alce65 <alejandro.cerezo@evosoft.es>
2021-07-26 11:20:57 +0200

    Change configuration and make initil tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'viewtest'`, () => {
+  it(`should have as title 'Viewnext Testing'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('viewtest');
+    expect(app.title).toEqual('Viewnext Testing');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('viewtest app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Viewnext Testing app is running!');
   });
 });
+
+
